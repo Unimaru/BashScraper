8a27843c0468b9ae6b5be628a877a53be3b5a91a
Unimaru <tbodorajava@gmail.com>
2018-06-18 11:46:09 +0200

    Added logger

--- src/main/java/pl/tb/Main.java
+++ src/main/java/pl/tb/Main.java
@@ -1,6 +1,8 @@
 package pl.tb;
 
 import org.apache.commons.cli.*;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import pl.tb.extraction.ExtractService;
 import pl.tb.extraction.WebData;
 import pl.tb.filehandling.SaveToFile;
@@ -9,15 +11,17 @@ import java.util.List;
 
 public class Main {
 
-//    private static final Logger logger = LogManager.getLogger("Main");
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws ParseException {
 
         Long limit = createLimitFromCLI(args);
 
         ExtractService extractService = new ExtractService();
+        logger.info("Starting parse bash.org.pl data limited to: " + limit + " pages.");
         List<WebData> webData = extractService.extractData(limit);
         new SaveToFile().save(webData);
+        logger.info("End parsing bash.org.pl.");
     }
 
     private static Long createLimitFromCLI(String[] args) throws ParseException {
